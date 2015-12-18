b2fe043cf5654e3cbfef25126e76955331e2bcaf
yassinenaitssi <naissi.y@gmail.com>
2015-12-18 12:29:05 +0100

    #46 - Création de la vue des paramètres

---
+++ LudoTech/src/gui/parameters/view/ParametersView.java
@@ -0,0 +1,24 @@
+package gui.parameters.view;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class ParametersView extends JPanel {
+
+	private JTextField nbBorrowings;
+	private JTextField nbBookings;
+	private JTextField durationofBorrowings;
+	private JTextField durationBetweenBookingandBorrowing;
+	private JTextField durationOfCotisation;
+	
+	private JButton validateButton;
+	private JButton cancelButton;
+	
+	
+	
+	public ParametersView() {
+		
+	}
+
+}
