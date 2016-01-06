e1e5b3870bb85193039d481dff29550c5914b5b2
Tombean <tom.somervilleroberts@gmail.com>
2016-01-06 16:54:07 +0100

    #45 la liste des membres s'affiche. GG WP

--- LudoTech/src/gui/members/controller/MembersController.java
+++ LudoTech/src/gui/members/controller/MembersController.java
@@ -37,30 +37,27 @@ import model.services.MemberContextServices;
 @SuppressWarnings("serial")
 public class MembersController extends JPanel {
 
-
 	private MemberListView memberListView;
 	private MemberSearchView memberSearchView;
-	
+
 	private int currentMemberID;
-	
+
 	private MemberServices memberServices;
 	private MemberContextServices memberContextServices;
-	
+
 	private MemberListModel memberListModel;
 	private ProfileView profileView;
-	
-	
-	
-	public MembersController( int currentMemberID) {
-		
+
+	public MembersController(int currentMemberID) {
+
 		this.currentMemberID = currentMemberID;
 		this.memberServices = new MemberServices();
 		this.memberContextServices = new MemberContextServices();
-		this.memberListModel = new MemberListModel( this.memberServices);
-		
+		this.memberListModel = new MemberListModel(this.memberServices);
+		this.setLayout(new BorderLayout());
 		this.makeGUI();
 	}
-	
+
 	public void makeGUI() {
 		boolean currentMemberIsAdmin = this.memberServices.isAdmin(this.currentMemberID);
 		this.memberSearchView = new MemberSearchView();
@@ -71,49 +68,67 @@ public class MembersController extends JPanel {
 		splitPane.setDividerLocation(LudoTechApplication.WINDOW_WIDTH / 4);
 		this.add(splitPane, BorderLayout.CENTER);
 	}
-	
+
 	private void makeListeners() {
 
-//		// Double clic sur une ligne -> Affichage et chargement des informations
-//		// du membre sélectionné dans la pop-up
-//		this.memberListView.getTable().addMouseListener(new MouseAdapter() {
-//			public void mouseClicked(MouseEvent e) {
-//				if (e.getClickCount() == 2) {
-//					JTable table = memberListView.getTable();
-//					int memberID = (Integer) table.getModel().getValueAt(table.getSelectedRow(), 0);
-//					Member member = memberServices.getMember(memberID);
-//					profileView.load(member.getFirstName(), member.getLastName(), member.getPseudo(), member.getPassword(),
-//							member.getIsAdmin(), member.getBirthDate(), member.getPhoneNumber(), member.getEmail(), member.getStreetAddress(),
-//							member.getPostalCode(), member.getCity());
-//					profileView.setVisible(true);
-//				}
-//			}
-//		});
-//		// Clic sur le bouton "chercher" sur la liste des membres
-//			this.memberSearchView.getSearchButton().addActionListener(new ActionListener() {
-//					public void actionPerformed(ActionEvent e) {
-//						refreshMemberList();
-//					}
-//				});
-//		
-//				this.memberListView.getAddGameButton().addActionListener(new ActionListener() {
-//					public void actionPerformed(ActionEvent e) {
-//						profileView.load("", "", "", "", false,  Calendar.getInstance().getTime(), "", "", "", "", "");
-//						profileView.setVisible(true);
-//					}
-//				});
-//				
-//				this.memberListView.getDeleteGameButton().addActionListener(new ActionListener() {
-//					public void actionPerformed(ActionEvent e) {
-//						JTable table = memberListView.getTable();
-//						int selectedRowIndex = table.getSelectedRow();
-//						if (selectedRowIndex > -1) {
-//							int memberID = (Integer) table.getModel().getValueAt(selectedRowIndex, 0);
-//							memberServices.removeMember(memberID);
-//							refreshGameList();
-//							}
-//						}
-//					});
-	
+		// // Double clic sur une ligne -> Affichage et chargement des
+		// informations
+		// // du membre sélectionné dans la pop-up
+		// this.memberListView.getTable().addMouseListener(new MouseAdapter() {
+		// public void mouseClicked(MouseEvent e) {
+		// if (e.getClickCount() == 2) {
+		// JTable table = memberListView.getTable();
+		// int memberID = (Integer)
+		// table.getModel().getValueAt(table.getSelectedRow(), 0);
+		// Member member = memberServices.getMember(memberID);
+		// profileView.load(member.getFirstName(), member.getLastName(),
+		// member.getPseudo(), member.getPassword(),
+		// member.getIsAdmin(), member.getBirthDate(), member.getPhoneNumber(),
+		// member.getEmail(), member.getStreetAddress(),
+		// member.getPostalCode(), member.getCity());
+		// profileView.setVisible(true);
+		// }
+		// }
+		// });
+		// // Clic sur le bouton "chercher" sur la liste des membres
+		// this.memberSearchView.getSearchButton().addActionListener(new
+		// ActionListener() {
+		// public void actionPerformed(ActionEvent e) {
+		// refreshMemberList();
+		// }
+		// });
+		//
+		// this.memberListView.getAddGameButton().addActionListener(new
+		// ActionListener() {
+		// public void actionPerformed(ActionEvent e) {
+		// profileView.load("", "", "", "", false,
+		// Calendar.getInstance().getTime(), "", "", "", "", "");
+		// profileView.setVisible(true);
+		// }
+		// });
+		//
+		// this.memberListView.getDeleteGameButton().addActionListener(new
+		// ActionListener() {
+		// public void actionPerformed(ActionEvent e) {
+		// JTable table = memberListView.getTable();
+		// int selectedRowIndex = table.getSelectedRow();
+		// if (selectedRowIndex > -1) {
+		// int memberID = (Integer)
+		// table.getModel().getValueAt(selectedRowIndex, 0);
+		// memberServices.removeMember(memberID);
+		// refreshGameList();
+		// }
+		// }
+		// });
+
+	}
+
+	public void refreshMemberList() {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				memberListModel.refresh();
+			}
+		});
+
 	}
 }
