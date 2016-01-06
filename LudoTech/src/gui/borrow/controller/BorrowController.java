e2d5d0178a4874beef2d45dafc5d2cb7f1841ac2
YvesLongchamp <longchampyves84@gmail.com>
2016-01-06 16:02:16 +0100

    [#42] - Emprunt
    Continuation du controller et du SearchView, avancement dans les
    services.

--- LudoTech/src/gui/borrow/controller/BorrowController.java
+++ LudoTech/src/gui/borrow/controller/BorrowController.java
@@ -4,7 +4,12 @@ import gui.LudoTechApplication;
 import gui.borrow.model.BorrowListModel;
 import gui.borrow.view.BorrowListView;
 import gui.borrow.view.BorrowSearchView;
+
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
 import javax.swing.SwingUtilities;
@@ -17,16 +22,17 @@ public class BorrowController extends JPanel {
 
 	private BorrowServices borrowServices;
 	private ItemServices itemServices;
-	
+
 	private BorrowSearchView borrowSearchView;
 	private BorrowListView borrowListView;
-	
+
 	private BorrowListModel borrowListModel;
 
 	public BorrowController() {
 		this.borrowServices = new BorrowServices();
 		this.itemServices = new ItemServices();
-		this.borrowListModel = new BorrowListModel(this.borrowServices, this.itemServices);
+		this.borrowListModel = new BorrowListModel(this.borrowServices,
+				this.itemServices);
 		this.setLayout(new BorderLayout());
 		this.makeGUI();
 	}
@@ -39,12 +45,31 @@ public class BorrowController extends JPanel {
 		splitPane.setDividerLocation(LudoTechApplication.WINDOW_WIDTH / 4);
 		this.add(splitPane, BorderLayout.CENTER);
 	}
-	
+
 	public void refreshBorrowList() {
+		/*SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				HashMap<String, String> filter = new HashMap<String, String>();
+				filter.put("game_name", borrowSearchView.getGamesNameValue().trim());
+				filter.put("member_name", borrowSearchView.getMembersNameValue().trim());
+				filter.put("ending_date", borrowSearchView.getEndingDateValue().trim());
+			}
+		});*/
+		
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				borrowListModel.refresh();
 			}
 		});
+	}
+
+	private void makeListeners() {
+		// Clic sur le bouton "chercher" sur la liste des emprunts
+		this.borrowSearchView.getSearchButton().addActionListener(
+				new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						refreshBorrowList();
+					}
+				});
 	}
 }
--- LudoTech/src/gui/borrow/view/BorrowSearchView.java
+++ LudoTech/src/gui/borrow/view/BorrowSearchView.java
@@ -2,11 +2,13 @@ package gui.borrow.view;
 
 import gui.LudoTechApplication;
 import gui.utils.PostLoadableComboBoxModel;
+import gui.utils.SpringUtilities;
 import gui.utils.TextView;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.util.Date;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -15,55 +17,116 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SpringLayout;
 
-public class BorrowSearchView extends JPanel{
+public class BorrowSearchView extends JPanel {
 	private JTextField gameNameField;
 	private JTextField memberNameField;
-	private JComboBox<String> stateBorrowComboBox; // recherche parmi les états (en retard, rendu, etc...)
-	//recherche par la date de rendu.
+	/*
+	 * private JComboBox<String> borrowStateComboBox; // recherche parmi les
+	 * états // (en retard, rendu, // etc...)
+	 */
+	private JTextField endingDateField; // recherche par la date de rendu.
 	private JTextField extensionNameField;
-	
+
 	private JButton searchButton;
-	
+
 	public BorrowSearchView() {
 		this.setLayout(new BorderLayout());
 		this.makeGUI();
 	}
-	
+
 	private void makeGUI() {
-		JLabel title = new JLabel(TextView.get("borrowListTitle"));
+		JLabel title = new JLabel(TextView.get("borrowSearchTitle"));
 		Font police = new Font("Arial", Font.BOLD, 16);
 		title.setFont(police);
 		title.setHorizontalAlignment(JLabel.CENTER);
 		this.add(title, BorderLayout.NORTH);
-		
+
 		JPanel searchPropertiesPanel = new JPanel(new SpringLayout());
-	
-		//Nom du jeu : 
+
+		// Nom du jeu :
 		JLabel gameNameLabel = new JLabel(TextView.get("borrowGameName"));
 		searchPropertiesPanel.add(gameNameLabel);
 		this.gameNameField = new JTextField();
-		this.gameNameField.setMaximumSize(new Dimension(LudoTechApplication.WINDOW_WIDTH, 20));
+		this.gameNameField.setMaximumSize(new Dimension(
+				LudoTechApplication.WINDOW_WIDTH, 20));
 		gameNameLabel.setLabelFor(this.gameNameField);
-		
-		//Nom du membre : 
+		searchPropertiesPanel.add(this.gameNameField);
+
+		// Nom du membre :
 		JLabel memberNameLabel = new JLabel(TextView.get("borrowMemberName"));
 		searchPropertiesPanel.add(memberNameLabel);
 		this.memberNameField = new JTextField();
-		this.memberNameField.setMaximumSize(new Dimension(LudoTechApplication.WINDOW_WIDTH, 20));
+		this.memberNameField.setMaximumSize(new Dimension(
+				LudoTechApplication.WINDOW_WIDTH, 20));
 		memberNameLabel.setLabelFor(this.memberNameField);
-		
-		// L'état de l'emprunt :
-		JLabel stateLabel = new JLabel(TextView.get("stateComboBox"));
-		searchPropertiesPanel.add(stateLabel);
-		this.stateBorrowComboBox = new JComboBox<String>();
-		this.stateBorrowComboBox.setModel(new PostLoadableComboBoxModel());
-		this.stateBorrowComboBox.setMaximumSize(new Dimension(LudoTechApplication.WINDOW_WIDTH, 20));
-		stateLabel.setLabelFor(this.stateBorrowComboBox);
-		searchPropertiesPanel.add(this.stateBorrowComboBox);
-		
+		searchPropertiesPanel.add(this.memberNameField);
+
+		/*
+		 * // L'état de l'emprunt : 
+		 * JLabel stateLabel = newJLabel(TextView.get("borrowStateComboBox"));
+		 * searchPropertiesPanel.add(stateLabel); 
+		 * this.borrowStateComboBox = newJComboBox<String>(); 
+		 * this.borrowStateComboBox.setModel(newPostLoadableComboBoxModel());
+		 * this.borrowStateComboBox.setMaximumSize(new Dimension(LudoTechApplication.WINDOW_WIDTH, 20));
+		 * stateLabel.setLabelFor(this.borrowStateComboBox);
+		 * searchPropertiesPanel.add(this.borrowStateComboBox);
+		 */
+
 		// Date de rendue :
+		JLabel dateLabel = new JLabel(TextView.get("borrowEndingDate"));
+		searchPropertiesPanel.add(dateLabel);
+		this.endingDateField = new JTextField();
+		this.endingDateField.setMaximumSize(new Dimension(
+				LudoTechApplication.WINDOW_WIDTH, 20));
+		dateLabel.setLabelFor(this.endingDateField);
+		searchPropertiesPanel.add(this.endingDateField);
+
 		// Nom de l'extension :
-		
-		
+		JLabel extensionNameLabel = new JLabel(
+				TextView.get("borrowExtensionName"));
+		searchPropertiesPanel.add(extensionNameLabel);
+		this.extensionNameField = new JTextField();
+		this.extensionNameField.setMaximumSize(new Dimension(
+				LudoTechApplication.WINDOW_WIDTH, 20));
+		extensionNameLabel.setLabelFor(this.extensionNameField);
+		searchPropertiesPanel.add(this.extensionNameField);
+
+		SpringUtilities
+				.makeCompactGrid(searchPropertiesPanel, 4, 2, 6, 6, 6, 6);
+
+		this.add(searchPropertiesPanel, BorderLayout.CENTER);
+
+		this.searchButton = new JButton(TextView.get("validate"));
+		this.add(this.searchButton, BorderLayout.SOUTH);
+	}
+
+	public String getGamesNameValue() {
+		return this.gameNameField.getText();
+	}
+
+	public String getMembersNameValue() {
+		return this.memberNameField.getText();
+	}
+
+	public void loadStates() {
+		// Renvoie la liste des états : En cours, rendu, en retard... etc
+		// services borrow à utiliser pour renvoyer la liste des états.
+	}
+
+	public String getEndingDateValue() {
+		return this.endingDateField.getText();
+	}
+
+	public JButton getSearchButton() {
+		return this.searchButton;
+	}
+
+	public void resetFields() {
+		this.gameNameField.setText("");
+		this.memberNameField.setText("");
+		//this.borrowStateComboBox.setSelectedIndex(-1);
+		this.endingDateField.setText("");
+		this.extensionNameField.setText("");
+
 	}
 }
